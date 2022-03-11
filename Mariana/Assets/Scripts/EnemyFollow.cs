44e232aec63b7aa559f801cc59a06046c61f4642
jabeleda1 <jabeleda2@knights.ucf.edu>
2022-03-11 15:57:49 -0500

    Added follow script to creature

--- Mariana/Assets/Scripts/EnemyFollow.cs
+++ Mariana/Assets/Scripts/EnemyFollow.cs
@@ -2,35 +2,34 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class EnemyController : MonoBehaviour
+public class EnemyFollow : MonoBehaviour
 {
     Rigidbody2D rigidbody2d;
-
     public float moveSpeed;
     public float stoppingDistance;
     float horizontal;
     float vertical;
+
     private Transform target;
+
+
     
     // Start is called before the first frame update
     void Start()
     {
-        rigidbody2d = GetComponent<Rigidbody2D>();
-        target = GameObject.FindGameObjectWithTag("Player").GetComponent<Transform>();
+       rigidbody2d = GetComponent<Rigidbody2D>();
+       target = GameObject.FindGameObjectWithTag("Player").GetComponent<Transform>(); 
     }
 
     // Update is called once per frame
-    void FixedUpdate()
+    void Update()
     {
+       Vector2 position = rigidbody2d.position;
+       rigidbody2d.MovePosition(position); 
+       
        if(Vector2.Distance(transform.position, target.position) > stoppingDistance)
        {
           transform.position = Vector2.MoveTowards(transform.position, target.position, moveSpeed * Time.deltaTime);
        } 
-       
-       Vector2 position = rigidbody2d.position;
-       position.x = position.x + moveSpeed * horizontal * Time.deltaTime;
-       position.y = position.y + moveSpeed * vertical * Time.deltaTime;
-       
-       rigidbody2d.MovePosition(position); 
     }
 }
