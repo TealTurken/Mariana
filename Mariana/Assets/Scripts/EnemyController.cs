9f0c8c571ab2f9093bb50ba952f73ce4a0ac20a1
Marcus0077 <marcus.werner@knights.ucf.edu>
2022-03-13 18:23:01 -0400

    Enemy 4 direction movement

--- Mariana/Assets/Scripts/EnemyController.cs
+++ Mariana/Assets/Scripts/EnemyController.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System;
 
 public class EnemyController : MonoBehaviour
 {
@@ -29,13 +30,25 @@ public class EnemyController : MonoBehaviour
     {
        if(Vector2.Distance(transform.position, target.position) > stoppingDistance)
        {
-          transform.position = Vector2.MoveTowards(transform.position, target.position, moveSpeed * Time.deltaTime);
-       } 
+           float xDifference = Math.Abs(transform.position.x - target.position.x);
+           float yDifference = Math.Abs(transform.position.y - target.position.y);
+
+           if (xDifference > yDifference)
+           {
+                Vector2 xTarget = new Vector2(target.position.x, transform.position.y);
+                transform.position = Vector2.MoveTowards(transform.position, xTarget, moveSpeed * Time.deltaTime);
+           }
+           else if (yDifference > xDifference)
+           {
+                Vector2 yTarget = new Vector2(transform.position.x, target.position.y);
+                transform.position = Vector2.MoveTowards(transform.position, yTarget, moveSpeed * Time.deltaTime);
+           }
+       }
        
        Vector2 position = rigidbody2d.position;
        position.x = position.x + moveSpeed * horizontal * Time.deltaTime;
        position.y = position.y + moveSpeed * vertical * Time.deltaTime;
        
-       rigidbody2d.MovePosition(position); 
+       rigidbody2d.MovePosition(position);
     }
 }
