53483ec19209814333f32fee32a633a7ba7d35ac
TealTurken <67345081+TealTurken@users.noreply.github.com>
2022-03-14 16:32:26 -0400

    Movement Speed update
    Movement speed is now derived from levels as opposed to being directly edited.
    -Added several speed variables (moveSpeed, sprintSpeed, normalSpeed, normalSprintSpeed, underwaterSpeed, UnderwaterSprintSpeed, sceneSpeed)

--- Mariana/Assets/Scripts/PlayerController.cs
+++ Mariana/Assets/Scripts/PlayerController.cs
@@ -3,22 +3,49 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using TMPro;  // is being used for debugging. To use, write "textbox.SetText("your text here");"
+using UnityEngine.SceneManagement;
+
 public class PlayerController : MonoBehaviour
 {
     Rigidbody2D rigidbody2d;
     Vector2 movement;
 
-    public float moveSpeed = 4.0f;
-    public TMP_Text textbox;
+    float moveSpeed; // actual movement speed applied to player character
+    float sprintSpeed; // actual sprint speed
+    public float normalSpeed = 4.0f;
+    public float normalSprintSpeed = 7.0f;
+    public float underwaterSpeed = 2.0f;
+    public float underwaterSprintSpeed = 4.5f;
+    float sceneSpeed; // retains scene movement speed when sprint speed is applied
     float horizontal;
     float vertical;
+    Scene newScene;
+    Scene activeScene;
    
     // Start is called before the first frame update
     void Start()
     {
         rigidbody2d = GetComponent<Rigidbody2D>();
-
-        moveSpeed = 4.0f;
+        #region Movement speed
+        newScene = SceneManager.GetActiveScene();
+        if (newScene.IsValid())
+        {
+            if (newScene != activeScene)
+            {
+                activeScene = newScene;
+                if (activeScene.name == "Scene")
+                {
+                    moveSpeed = normalSpeed; // Normal move speed
+                    sprintSpeed = normalSprintSpeed;
+                }
+                if (activeScene.name == "Water1")
+                {
+                    moveSpeed = underwaterSpeed; // underwater move speed
+                    sprintSpeed = underwaterSprintSpeed;
+                }
+            }
+        }
+        #endregion
     }
 
     // Update is called once per frame
@@ -44,12 +71,13 @@ public class PlayerController : MonoBehaviour
         #region Sprint
         if (Input.GetKeyDown(KeyCode.LeftShift))
        {
-           moveSpeed = 7.0f;
+           sceneSpeed = moveSpeed;
+           moveSpeed = sprintSpeed;
        }
 
        if (Input.GetKeyUp(KeyCode.LeftShift))
        {
-           moveSpeed = 4.0f;
+           moveSpeed = sceneSpeed;
        }
         #endregion
 
